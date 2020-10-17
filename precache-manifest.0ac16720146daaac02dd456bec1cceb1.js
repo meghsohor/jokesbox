3ffb29e7409dfe71d6aa6e47a2b52a763eb9df28
Shuvo <meghsohor@yahoo.com>
2020-10-17 19:58:22 +0600

    Updates

---
+++ precache-manifest.0ac16720146daaac02dd456bec1cceb1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "da180f35435b835bcee1af1c8722a92c",
+    "url": "/jokesbox/index.html"
+  },
+  {
+    "revision": "aeb3b78795a04ddb040f",
+    "url": "/jokesbox/static/css/main.5fe52bf9.chunk.css"
+  },
+  {
+    "revision": "9214573bab642eb1909b",
+    "url": "/jokesbox/static/js/2.dc59ce34.chunk.js"
+  },
+  {
+    "revision": "b3e2770e6336acd821638b70a2a55e3e",
+    "url": "/jokesbox/static/js/2.dc59ce34.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aeb3b78795a04ddb040f",
+    "url": "/jokesbox/static/js/main.71413a9c.chunk.js"
+  },
+  {
+    "revision": "20dbc6d9b2cc30006a6d",
+    "url": "/jokesbox/static/js/runtime-main.176fd7b8.js"
+  }
+]);
